1c233376de404a1c2e91729d75ca758547f00c0b
Juan Pablo Tovar <juan.tovar@epn.edu.ec>
2022-05-10 18:38:40 -0500

    Fix / fix path in next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    basePath: '/https://juancho1502011.github.io/Portafolio',
-    assetPrefix: '/https://juancho1502011.github.io/Portafolio',
+    basePath: '/Portafolio',
+    assetPrefix: '/Portafolio'
 }
