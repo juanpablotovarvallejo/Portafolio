17439994fb3889ee72d44d844a463c617cf9627c
Juancho1502011 <72332156+Juancho1502011@users.noreply.github.com>
2022-05-10 23:48:55 +0000

    Deploying to gh-pages from @ Juancho1502011/Portafolio@5ff7b58c63b16f841c70336d3abb04b8c0394ac2 🚀

---
+++ _next/static/BoAIMXyCoWMRpEQV3sbff/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0c428ae2-c61241e863a9468d.js","static/chunks/686-69ec1444be54df14.js","static/chunks/pages/index-902132083606a46d.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
